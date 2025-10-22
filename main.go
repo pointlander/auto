cc07f19405aadfd35c15c1c1127f3487e831de6b
Andrew Snodgrass <pointlander@gmail.com>
2025-10-22 15:41:56 -0500

    first commit

---
+++ main.go
@@ -0,0 +1,53 @@
+// Copyright 2025 The Auto Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"compress/bzip2"
+	"embed"
+	"fmt"
+	"io"
+)
+
+//go:embed books/*
+var Text embed.FS
+
+func main() {
+	type File struct {
+		Name string
+		Data []byte
+	}
+
+	files := []File{
+		{Name: "10.txt.utf-8.bz2"},
+		{Name: "pg74.txt.bz2"},
+		{Name: "76.txt.utf-8.bz2"},
+		{Name: "84.txt.utf-8.bz2"},
+		{Name: "100.txt.utf-8.bz2"},
+		{Name: "1837.txt.utf-8.bz2"},
+		{Name: "2701.txt.utf-8.bz2"},
+		{Name: "3176.txt.utf-8.bz2"},
+	}
+
+	load := func(book *File) {
+		path := fmt.Sprintf("books/%s", book.Name)
+		file, err := Text.Open(path)
+		if err != nil {
+			panic(err)
+		}
+		defer file.Close()
+		breader := bzip2.NewReader(file)
+		data, err := io.ReadAll(breader)
+		if err != nil {
+			panic(err)
+		}
+		book.Data = data
+	}
+
+	for i := range files {
+		load(&files[i])
+		fmt.Println(files[i].Name)
+	}
+}
